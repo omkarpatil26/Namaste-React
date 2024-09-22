d1dc38a33382d50d8691b1964b99f3e4a28719bb
omkar <omkarpatil26498@gmail.com>
2024-09-22 14:31:38 +0530

    Updates

---
+++ static/js/70.afca5113.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknamaste_react=self.webpackChunknamaste_react||[]).push([[70],{70:function(e,a,n){n.r(a);var r=n(579);a.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"we can bundle grocery - swiggy instamart app here!"})})}}}]);
+//# sourceMappingURL=70.afca5113.chunk.js.map
